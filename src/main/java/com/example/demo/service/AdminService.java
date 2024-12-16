d24f3c7a7a90a0d28686d95b360d8e3038cdf3d5
Vaxee03 <012jas84@gmail.com>
2024-12-16 17:45:16 +0900

    Refactor : reportUsers 메서드 개선
    기존에 상태 변경 N개 당 findById / save 로 2N번의 DB 접근에서
    한 번에 조회하고 한 번에 변경하여 2번의 DB 접근으로 개선

--- src/main/java/com/example/demo/service/AdminService.java
+++ src/main/java/com/example/demo/service/AdminService.java
@@ -18,12 +18,16 @@ public class AdminService {
     // TODO: 4. find or save 예제 개선
     @Transactional
     public void reportUsers(List<Long> userIds) {
-        for (Long userId : userIds) {
-            User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
+        // 한 번의 쿼리로 모든 사용자 조회
+        List<User> users = userRepository.findAllById(userIds);
 
-            user.updateStatusToBlocked();
-
-            userRepository.save(user);
+        if (users.size() != userIds.size()) {
+            throw new IllegalArgumentException("일부 사용자 ID에 해당하는 값이 존재하지 않습니다.");
         }
+
+        // 상태를 변경
+        users.forEach(User::updateStatusToBlocked);
+
+        // 자동 저장
     }
 }
