a4d3948841acd7b059d9993599a54ba5ee0636b6
Vaxee03 <012jas84@gmail.com>
2024-12-16 15:49:52 +0900

    Refactor : createReservation 메서드 개선
    @Transactional 어노테이션 추가
    (트랜잭션 관리를 통해 DB의 원자성 및 일관성 보장)

--- src/main/java/com/example/demo/service/ReservationService.java
+++ src/main/java/com/example/demo/service/ReservationService.java
@@ -35,18 +35,21 @@ public class ReservationService {
     }
 
     // TODO: 1. 트랜잭션 이해
+    @Transactional
     public void createReservation(Long itemId, Long userId, LocalDateTime startAt, LocalDateTime endAt) {
-        // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
-        List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
-        if(!haveReservations.isEmpty()) {
-            throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
-        }
+//        // 쉽게 데이터를 생성하려면 아래 유효성검사 주석 처리
+//        List<Reservation> haveReservations = reservationRepository.findConflictingReservations(itemId, startAt, endAt);
+//        if(!haveReservations.isEmpty()) {
+//            throw new ReservationConflictException("해당 물건은 이미 그 시간에 예약이 있습니다.");
+//        }
 
+        // 예약 생성 로직
         Item item = itemRepository.findById(itemId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
         User user = userRepository.findById(userId).orElseThrow(() -> new IllegalArgumentException("해당 ID에 맞는 값이 존재하지 않습니다."));
         Reservation reservation = new Reservation(item, user, "PENDING", startAt, endAt);
         Reservation savedReservation = reservationRepository.save(reservation);
 
+        // 렌탈 로그 생성 및 저장
         RentalLog rentalLog = new RentalLog(savedReservation, "로그 메세지", "CREATE");
         rentalLogService.save(rentalLog);
     }
